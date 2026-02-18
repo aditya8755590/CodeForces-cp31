07ab5c48be472d0024d45198e5ee345fce8f6460
aditya8755590 <aditya.kumar875590@gmail.com>
2026-02-18 16:25:24 +0530

    Add ArrayColoring and SegmentTree classes for array manipulation and segment tree operations

---
+++ 800-Rating/ArrayColoring.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ArrayColoring {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        while(n>0){
+            int size=sc.nextInt();
+            int[] arr=new int[size];
+            sol(arr,size);
+            n--;
+        }
+    }
+    public static void sol(int[] arr,int n){
+        
+    }
+}
